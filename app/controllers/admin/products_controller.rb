65f940824a47f3af392e9daa3de06283e67aaf34
zhuwenqian <1506675573@qq.com>
2016-08-27 11:38:22 +0800

    now edit/create/new works in /admin/products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -7,18 +7,36 @@ class Admin::ProductsController < Admin::BaseController
     @product = Product.new
   end
 
-  def crete
+  def create
+    @product = Product.new params_product
+    if @product.save
+      redirect_to [:admin, :products]
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @product = Product.find params[:id]
   end
 
   def update
+    @product = Product.find params[:id]
+    if @product.update_attributes params_product
+      redirect_to [:admin, :products]
+    else
+      render 'edit'
+    end
   end
 
   def destroy
   end
 
   def show
+  end
+
+  private
+  def params_product
+    params.require(:product).permit(:sku, :title, :feature, :desc_as_option, :is_main_body, :is_option, :is_new, :is_recommended, :banner, :summary)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
+require File.join(Rails.root, "app/uploaders/avatar_uploader.rb")
 class Product < ApplicationRecord
+  mount_uploader :banner, AvatarUploader
 end
