5f941c3ce582e909cd2684192f4e4a82a170fa8f
wenbo <yiyun6674@hotmail.com>
2016-08-29 18:09:10 +0800

    +  def parse_date(date,join = '-')

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 # coding: utf-8
 module ApplicationHelper
+  
+  def parse_date(date,join = '-') 
+    date.strftime("%Y#{join}%m#{join}%d") if date
+  end
+  
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
