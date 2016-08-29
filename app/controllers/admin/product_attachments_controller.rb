4c7079033b7ef673e36b7ad72f1ad95eb19c4837
wenbo <yiyun6674@hotmail.com>
2016-08-29 17:40:08 +0800

    new file:   app/views/admin/product_attachments/_product_attachment.html.erb

--- app/controllers/admin/product_attachments_controller.rb
+++ app/controllers/admin/product_attachments_controller.rb
@@ -1,22 +1,37 @@
 class Admin::ProductAttachmentsController < Admin::BaseController
   def index
+    @product_attachments = ProductAttachment.page(params[:page]).per(20)
   end
 
   def new
+    @product_attachment = ProductAttachment.new
   end
 
   def create
+    @product_attachment = ProductAttachment.new params_product_attachment
+    @product_attachment.pdf = params[:product_attachment][:pdf]
+    if @product_attachment.save
+      redirect_to [:admin, :industries]
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @product_attachment = ProductAttachment.find params[:id]
   end
 
   def update
+    @product_attachment = ProductAttachment.find params[:id]
+    if @product_attachment.update_attributes params_product_attachment
+      redirect_to [:admin, :product_attachments]
+    else
+      render 'edit'
+    end
   end
 
-  def destroy
-  end
-
-  def show
+  private
+  def params_product_attachment
+    params.require(:product_attachment).permit(:name, :note, :pdf_category_id)
   end
 end
--- app/models/product_attachment.rb
+++ app/models/product_attachment.rb
@@ -1,3 +1,5 @@
 class ProductAttachment < ApplicationRecord
   belongs_to :pdf_category
+  has_attached_file :pdf, styles: {thumbnail: ["60x60#", :png]}
+  validates_attachment :pdf, content_type: { content_type: "application/pdf" }
 end
