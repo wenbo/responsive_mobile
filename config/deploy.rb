56d8766cf312ab223d1312306b05aa0a15df8427
watase <watase@kembo-net.com>
2016-09-06 00:25:46 +0800

    modified:   app/assets/stylesheets/application.css 	new file:   config/deploy.rb 	new file:   config/deploy/production.rb 	new file:   config/deploy/staging.rb

---
+++ config/deploy.rb
@@ -0,0 +1,12 @@
+lock '3.6.1'
+
+set :application, 'hioki_rails'
+set :repo_url, 'git@gitlab.com:hioki/hiroki_rails.git'
+
+set :keep_releases, 5
+
+set :rbenv_ruby, '2.3.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all 
+
---
+++ config/deploy/production.rb
@@ -0,0 +1,12 @@
+set :branch, 'master'
+set :deploy_to, '/home/admin/www/hioki_rails/production'
+set :bundle_flags, "--no-binstubs"
+set :log_level, :debug
+
+role :app, %w{222.73.5.207}
+role :web, %w{222.73.5.207}
+role :db,  %w{222.73.5.207}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets bundle  public/assets public/uploads public/projects}
+set :linked_files, %w{  puma.rb config/database.yml  }
+
+server '222.73.5.207',port: 22, user: 'admin', roles: %w{web app db}
