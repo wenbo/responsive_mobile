ddb5ba62dd3f8241536cb18f9d8fa67e6b1bd0b6
wenbo <yiyun6674@hotmail.com>
2016-09-07 15:13:11 +0800

    +  validates_attachment_content_type :data, content_type: [/\Aimage/, application/octet-stream]

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -6,7 +6,7 @@ class Ckeditor::Picture < Ckeditor::Asset
 
   validates_attachment_presence :data
   validates_attachment_size :data, less_than: 2.megabytes
-  validates_attachment_content_type :data, content_type: /\Aimage/
+  validates_attachment_content_type :data, content_type: [/\Aimage/, "application/octet-stream"]
 
   def url_content
     url(:content)
