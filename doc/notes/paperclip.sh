275767121a983019e15358d8b489c6ee65136523
wenbo <yiyun6674@hotmail.com>
2016-09-07 14:16:54 +0800

    new file:   config/initializers/paperclip_media_type_spoof_detector_override.rb

---
+++ doc/notes/paperclip.sh
@@ -0,0 +1,4 @@
+[paperclip] Content Type Spoof
+Command :: 
+file -b --mime '/tmp/5abf64df462df96a406eb6f00787e03a20160907-9443-zh661s.png'
+file -b --mime ./public/apple-touch-icon-precomposed.png 
