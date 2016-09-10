f1a185aa9b7d02bc21d0468e294704de1c27a97a
zhuwenqian <1506675573@qq.com>
2016-09-10 16:17:13 +0800

    +    params.require(:product).permit(:sku, :title, :feature, :note_for_option, :desc

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -40,7 +40,7 @@ class Admin::ProductsController < Admin::BaseController
 
   private
   def params_product
-    params.require(:product).permit(:sku, :title, :feature, :desc_as_option, :is_main_body, :is_option, :is_new, :is_recommended, :is_display, :is_deleted, :banner, :thumb_image, :summary, :category_id, option_categories_attributes: [:name, :note, :option_sku_collection, :_destroy, :id], industry_ids: [])
+    params.require(:product).permit(:sku, :title, :feature, :note_for_option, :desc, :is_main_body, :is_option, :is_new, :is_recommended, :is_display, :is_deleted, :banner, :thumb_image, :summary, :category_id, option_categories_attributes: [:name, :note, :option_sku_collection, :_destroy, :id], industry_ids: [])
   end
 
   def sort_column
