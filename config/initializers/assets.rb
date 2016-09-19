65b917c8502bfe92a1da3bb3df4794d4fd734898
wenbo <yiyun6674@hotmail.com>
2016-09-19 14:50:29 +0800

    now view of home/index looks good

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( login.js ckeditor/* )
-Rails.application.config.assets.precompile += %w( login.css )
+Rails.application.config.assets.precompile += %w( login.js ckeditor/* scrollsmoothly.js jquery-1.9.1 )
+Rails.application.config.assets.precompile += %w( login.css style.css )
