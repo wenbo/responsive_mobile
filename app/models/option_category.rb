15ce48c9ec5a7b6b1903a933fd0a527124cf028b
wenbo <yiyun6674@hotmail.com>
2016-09-21 21:23:41 +0800

    new file:   app/views/products/_options.html.erb

--- app/models/option_category.rb
+++ app/models/option_category.rb
@@ -1,3 +1,13 @@
 class OptionCategory < ApplicationRecord
   belongs_to :product
+  
+  def options
+    skus = self.option_sku_collection
+    options = []
+    skus.split(",").each do |sku|
+      option = Product.find_by_sku(sku)
+      options << option if option.present?
+    end
+    options
+  end
 end
