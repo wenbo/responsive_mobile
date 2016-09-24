bff0ebb739a9ea89b6c52b769821db01054f1462
zhuwenqian <1506675573@qq.com>
2016-09-24 17:56:47 +0800

    added app/views/products/_case.html.erb, _options.html.erb, _related_products.html.erb, _upgrade.html.erb

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
 class NewsController < ApplicationController
   def index
-    @news = News.is_public.order("public_time desc").limit(6)
+    @news = News.is_public.order("public_time desc").page(params[:page]).per(6)
   end
 
   def show
