0d694048e0fe21997298b7c62a3b3b02429cb490
wenbo <yiyun6674@hotmail.com>
2016-09-26 17:22:15 +0800

    new file:   vendor/assets/stylesheets/jquery-ui.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( login.js ckeditor/* scrollsmoothly.js jquery-1.9.1 tab )
-Rails.application.config.assets.precompile += %w( login.css style.css )
+Rails.application.config.assets.precompile += %w( login.js ckeditor/* scrollsmoothly.js jquery-1.9.1 tab jquery-ui )
+Rails.application.config.assets.precompile += %w( login.css style.css jquery-ui.css )
