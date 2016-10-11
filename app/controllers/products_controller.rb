1ae20b6bfc26ae3e8f25b02af523ab5d4832d04c
wenbo <yiyun6674@hotmail.com>
2016-10-11 14:53:56 +0800

    +      @products = Product.search(params[:search]).is_display.is_main_body

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
   def index
     if params[:search].present?
-      @products = Product.search(params[:search]).is_display.is_main_body.page(params[:page]).per(20)
+      @products = Product.search(params[:search]).is_display.is_main_body
     else
-      @products = Product.is_display.is_main_body.page(params[:page]).per(20)
+      @products = Product.is_display.is_main_body
     end
     @root_categories = Category.roots
   end
