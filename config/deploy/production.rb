0cb3eece1c89d68d30ed8843538bde4dc5e637ee
wenbo <yiyun6674@hotmail.com>
2016-10-12 17:22:00 +0800

    +set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets bundle  public/assets public/uploads public/projects public/ckeditor_assets public/system}

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@ set :log_level, :debug
 role :app, %w{222.73.5.207}
 role :web, %w{222.73.5.207}
 role :db,  %w{222.73.5.207}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets bundle  public/assets public/uploads public/projects}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets bundle  public/assets public/uploads public/projects public/ckeditor_assets public/system}
 set :linked_files, %w{  puma.rb config/database.yml  }
 
 server '222.73.5.207',port: 22, user: 'admin', roles: %w{web app db}
