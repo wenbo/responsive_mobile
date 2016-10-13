fef80bb9d91d1ae2a5c4b59c1598af09980ab920
wenbo <yiyun6674@hotmail.com>
2016-10-13 14:18:55 +0800

    +    @industries = Industry.ordered.roots, +  scope :ordered, -> {order('id asc')}

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -3,6 +3,6 @@ class IndustriesController < ApplicationController
     # @industry = Industry.find params[:id]
     # @root_categories = Category.roots
     # @root_categories = @industry.categories
-    @industries = Industry.find([1,2,3,4,5])
+    @industries = Industry.ordered.roots
   end
 end
--- app/models/industry.rb
+++ app/models/industry.rb
@@ -2,4 +2,5 @@ class Industry < ApplicationRecord
   #validates :name, uniqueness: true
   acts_as_tree order: "name"
   has_and_belongs_to_many :categories
+  scope :ordered, -> {order('id asc')}
 end
