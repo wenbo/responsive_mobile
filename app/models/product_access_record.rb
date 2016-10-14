d7b46b322ba1635399dcae7b619f79782521af38
wenbo <yiyun6674@hotmail.com>
2016-10-14 10:55:55 +0800

    self_and_descendants_id)]).order('visited_count desc').limit(10) }

--- app/models/product_access_record.rb
+++ app/models/product_access_record.rb
@@ -1,5 +1,5 @@
 class ProductAccessRecord < ApplicationRecord
-  scope :category_most_visited_products, -> (category) {  where(["product_access_records.category_id in (?)", (category.self_and_descendants_id)]).order('visited_count desc').limit(4) }  
+  scope :category_most_visited_products, -> (category) {  where(["product_access_records.category_id in (?)", (category.self_and_descendants_id)]).order('visited_count desc').limit(10) }  
   belongs_to :product
   belongs_to :category
 end
