8d1cf9a97429048a3c12832be63601d33500a6b5
wenbo <yiyun6674@hotmail.com>
2016-10-15 10:40:06 +0800

    +  do_not_validate_attachment_file_type :attachment

--- app/models/upgrade_attachment.rb
+++ app/models/upgrade_attachment.rb
@@ -1,7 +1,8 @@
 class UpgradeAttachment < ApplicationRecord
   belongs_to :product
   has_attached_file :attachment, styles: {thumbnail: ["60x60#", :png]}
-  validates_attachment :attachment, content_type: { content_type: ["application/pdf", "application/zip"] }
+  # validates_attachment :attachment, content_type: { content_type: ["application/pdf", "application/zip"] }
+  do_not_validate_attachment_file_type :attachment
   before_post_process :skip_for_zip
 
   def skip_for_zip
