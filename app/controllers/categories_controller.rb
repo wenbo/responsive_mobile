f004c3097e41f1295fd70bcebb1e289b8cb6ac00
bob <yiyun6674@hotmail.com>
2016-11-22 14:46:05 +0800

    +                        <a href="/categories/<%= r_c.id %>?industry_id=<%= industry.id %>">

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find params[:id]
+    industry_ids = Industry.ordered.roots[params[:industry_id].to_i-1].children.map(&:id)
+    puts "industry_ids"
+    puts industry_ids.inspect
     @category_parent = @category.parent
     if params[:search].present?
       @products = Product.category_all_products(@category).search(params[:search]).ordered.is_display.is_main_body.page(params[:page])
--- app/models/industry.rb
+++ app/models/industry.rb
@@ -2,5 +2,6 @@ class Industry < ApplicationRecord
   #validates :name, uniqueness: true
   acts_as_tree order: "name"
   has_and_belongs_to_many :categories
+  has_and_belongs_to_many :products
   scope :ordered, -> {order('id asc')}
 end
