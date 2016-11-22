f735eb835aa1d2fa7cd09d7b8279a9a4b01667ee
bob <yiyun6674@hotmail.com>
2016-11-22 16:34:36 +0800

    +    @products = (@products & @products_in_industry) if @products_in_industry.present?

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,18 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find params[:id]
-    industry_ids = Industry.ordered.roots[params[:industry_id].to_i-1].children.map(&:id)
-    puts "industry_ids"
-    puts industry_ids.inspect
+    if params[:industry_id].present?
+      industry_ids = Industry.find(params[:industry_id]).children.map(&:id)
+      @products_in_industry = industry_ids.collect do |industry_id| Industry.find(industry_id).products end.flatten
+    end
+
     @category_parent = @category.parent
     if params[:search].present?
-      @products = Product.category_all_products(@category).search(params[:search]).ordered.is_display.is_main_body.page(params[:page])
+      @products = Product.category_all_products(@category).search(params[:search]).ordered.is_display.is_main_body
     else
-      @products = Product.category_all_products(@category).ordered.is_display.is_main_body.page(params[:page]).per(20)
+      @products = Product.category_all_products(@category).ordered.is_display.is_main_body
     end
+    @products = (@products & @products_in_industry) if @products_in_industry.present?
     @root_categories = Category.roots
   end
 end
