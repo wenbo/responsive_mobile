54be79f3346ca7680662ced7ff83dd67df693316
bob <yiyun6674@hotmail.com>
2016-11-24 21:23:40 +0800

    public/userscenter/footer.php, public/userscenter/header.php

---
+++ public/userscenter/footer.php
@@ -0,0 +1,59 @@
+<p class="hioki_page"><a href="#top"><img src="/userscenter/images/hioki_r28_c37.jpg" width="60" height="57" /></a></p>
+<div class="hioki_column">
+  <div class="hioki_search clearfix">
+    <div class="hioki_column01">
+      <h3 class="hioki_f23">业界商品分类 <span class="hioki_f15">Search Applications</span></h3>
+      <ul class="m_top25">
+        <li>&gt; <a href="/industries/1">电力/新能源 <span class="hioki_f12">Power, Energy, Environment</span></a></li>
+        <li class="m_top10">&gt; <a href="/industries/2">汽车 <span class="hioki_f12">Automotive, Transport</span></a></li>
+        <li class="m_top10">&gt; <a href="/industries/3">电子零件 <span class="hioki_f12">Components, Semiconductors</span></a></li>
+        <li class="m_top10">&gt; <a href="/industries/4">电池 <span class="hioki_f12">Battery</span></a></li>
+        <li class="m_top10">&gt; <a href="/industries/5">交通（地铁、铁道、航空）<br />　<span class="hioki_f12">Underground, Railway, Aviation</span></a></li>
+        <li>&nbsp;</li>
+      </ul>
+    </div>
+    <div class="hioki_column02 clearfix">
+      <h3 class="hioki_f23">日置产品一览 <span class="hioki_f15">Search Category</span></h3>
+      <ul class="hioki_w205 m_top25">
+				<li class="m_top10">&gt; <a href="/categories/31">记录仪,数据采集仪</a></li>
+				<li class="m_top10">&gt; <a href="/categories/23">电子测量仪表</a></li>
+				<li class="m_top10">&gt; <a href="/categories/14">安全标准测量仪表</a></li>
+				<li class="m_top10">&gt; <a href="/categories/18">电力测量仪器</a></li>
+				<li class="m_top10">&gt; <a href="/categories/28">钳形表,钳形传感器</a></li>
+			</ul>
+			<ul class="fl m_top25">
+				<li class="m_top10">&gt; <a href="/categories/34">信号发生器,波形发生器</a></li>
+				<li class="m_top10">&gt; <a href="/categories/10">环境测量仪表</a></li>
+				<li class="m_top10">&gt; <a href="/categories/1">现场测试仪器</a></li>
+				<li class="m_top10">&gt; <a href="/categories/40">自动测试设备</a></li>
+			</ul>
+
+    </div>
+    <div class="hioki_column03">
+      <h3 class="hioki_f23">关于日置 <span class="hioki_f15">About HIOKI</span></h3>
+      <ul class="m_top25">
+        <li>&gt; 关于社长</li>
+        <li class="m_top10">&gt; HIOKI的企业目标</li>
+        <li class="m_top10">&gt; 事业内容</li>
+        <li class="m_top10">&gt; 环境・CSR</li>
+        <li class="m_top10">&gt; 公司概要</li>
+        <li class="m_top10">&gt; 公司分布・分公司</li>
+        <li>&nbsp;</li>
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="footer clearfix">
+  <h1 class="fl"><img width="148" height="30" src="/userscenter/images/hioki_r30_c2.jpg" alt="Hioki r30 c2" /></h1>
+  <p class="footer_dh"><img width="17" height="17" src="/userscenter/images/hioki_r31_c14.jpg" alt="Hioki r31 c14" /> 021-63343307/3308</p>
+  <p class="footer_dh"><img width="15" height="12" src="/userscenter/images/hioki_r32_c18.jpg" alt="Hioki r32 c18" /> info@hioki.com.cn</p>
+  <p class="footer_dh">中国区维修服务中心（日置(上海) 商贸有限公司）</p>
+  <ul class="fr">
+    <li><a href="/">&gt; 首　页</a></li>
+    <li><a href="/news.html">&gt; 新　闻</a></li>
+    <li><a href="/product.html">&gt; 产品一览</a></li>
+    <li><a href="/company.html">&gt; 公司介绍</a></li>
+    <li><a href="/contact.html">&gt; 联系我们</a></li>
+  </ul>
+  <p class="footer_icp clear">沪ICP备05013343号</p>
+</div>
---
+++ public/userscenter/header.php
@@ -0,0 +1,30 @@
+<a name="top" id="top"></a>
+<div class="top_bg">
+  <div class="top clearfix">
+	<p class="fl">中国区维修服务中心（日置(上海) 商贸有限公司）</p>
+    <p class="fr"><a href="#"><img src="/userscenter/images/hioki_r1_c30.jpg" width="28" height="26" />会员退出</a>　<img src="/userscenter/images/hioki_r1_c32.jpg" width="22" height="26" />400-920-6010</p>
+  </div>
+</div>
+<div class="header">
+	 <h1 class="fl"><a href="/"><img width="150" height="30" alt="HIOKI" src="/userscenter/images/hioki_r4_c3.jpg" /></a></h1>
+	 <ul>
+	 <li class="nav_li">
+	 <a href="/">首　　页</a>
+	 </li>
+	 <li>
+	 <a href="/news.html">新　　闻</a>
+	 </li>
+	 <li>
+	 <a href="/industries/1/products">产品一览</a>
+	 </li>
+	 <li>
+	 <a href="/company.html">公司介绍</a>
+	 </li>
+	 <li>
+	 <a href="/contact.html">联系我们</a>
+	 </li>
+	 <li class="nav_li02">
+	 <a href="/repair.html">维修中心</a>
+	 </li>
+	 </ul>
+	 </div>
