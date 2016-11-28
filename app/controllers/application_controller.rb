a89d1fe85514bf7630ba560e53d89e270fbbf701
bob <yiyun6674@hotmail.com>
2016-11-28 19:12:07 +0800

    +    if session[:user_name].blank? && login_stub.present?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :a_name, :c_name
+  before_filter :check_cookie
 
   def a_name; action_name end
   def c_name; controller_name end
+
+  def check_cookie
+    # puts "************ login_stub ************"
+    login_stub = cookies[:login_stub]
+    if session[:user_name].blank? && login_stub.present?
+      decoded = Base64.decode64(login_stub)
+      session[:user_id], session[:user_name] = decoded.split(',')
+    end
+  end
 end
