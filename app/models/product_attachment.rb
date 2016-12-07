45834f51c7b136921a79c6d78749b7ec603dfeef
zhuwenqian <1506675573@qq.com>
2016-12-07 18:54:28 +0800

    +  do_not_validate_attachment_file_type :pdf

--- app/models/product_attachment.rb
+++ app/models/product_attachment.rb
@@ -1,6 +1,12 @@
 class ProductAttachment < ApplicationRecord
   belongs_to :pdf_category
   has_attached_file :pdf, styles: {thumbnail: ["60x60#", :png]}
-  validates_attachment :pdf, content_type: { content_type: "application/pdf" }
+  #validates_attachment :pdf, content_type: { content_type: "application/pdf" }
   has_and_belongs_to_many :products
+  do_not_validate_attachment_file_type :pdf
+  before_post_process :skip_for_zip
+
+  def skip_for_zip
+    !%w(application/zip application/x-zip application/pdf application/octet-stream).include?(pdf_content_type)
+  end
 end
