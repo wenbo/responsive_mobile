b1a12967a8f66b2c86fcbce430b80b361b8bd536
zhuwenqian <1506675573@qq.com>
2016-12-16 14:02:33 +0800

    +    coffee-script-source (1.11.1)， +  default_scope  -> {order('public_time DESC')}

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,5 @@
 class News < ApplicationRecord
   belongs_to :news_category
-  default_scope  -> {order('created_at DESC')}
+  default_scope  -> {order('public_time DESC')}
   scope :is_public, -> { where(is_public: true) }
 end
