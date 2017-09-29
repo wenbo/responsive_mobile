30498a09c86c8acb32a879a88af38d57c9666de3
Bo Wen <b-wen@ruby-dev.jp>
2017-09-29 13:32:16 +0900

    +  if ( window.confirm('确认注册产品信息吗?') )

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -6,6 +6,6 @@ class Admin::RegistrationsController < Admin::BaseController
   # http://ruby-rails.hatenadiary.com/entry/20141119/1416398472
   def export
    require 'csv' 
-   send_data Registration.to_csv, filename: "data_on_#{Time.now.strftime('%Y%m%d%H%M')}.csv"
+   send_data Registration.to_csv, filename: "data_on_#{Time.now.strftime('%Y%m%d%H%M')}.csv", type: 'text/csv; charset=utf-8'
   end
 end
