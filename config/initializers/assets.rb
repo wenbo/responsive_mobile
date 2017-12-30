9fa35011f1bf96414cc58f7b78521a2ac001f326
zhuwenqian <1506675573@qq.com>
2017-12-30 23:56:24 +0800

    deleted:    app/assets/stylesheets/pad.css.erb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( login.js ckeditor/* scrollsmoothly.js jquery-1.9.1 tab jquery-ui main  Effect common default jquery script tabslide bootstrap.min jquery.colorbox jquery_ujs)
+Rails.application.config.assets.precompile += %w( login.js ckeditor/* scrollsmoothly.js jquery-1.9.1 tab jquery-ui main jquery script tabslide bootstrap.min jquery.colorbox jquery_ujs)
 Rails.application.config.assets.precompile += %w(ckeditor/config.js)
-Rails.application.config.assets.precompile += %w( login.css style.css pad.css sp.css jquery-ui.css colorbox.css)
+Rails.application.config.assets.precompile += %w( login.css style.css jquery-ui.css colorbox.css)
--- config/initializers/intro_links.rb
+++ config/initializers/intro_links.rb
@@ -1,7 +1,7 @@
 IntroLinks = {
   "31"=> {
-    "存储记录仪的原理" => "https://www.hioki.cn/html/methods/rec.html",
-    "数据采集仪的使用方法" => "https://www.hioki.cn/html/methods/rec_method.html"
+    "数据采集仪的使用方法" => "https://www.hioki.cn/html/methods/rec_method.html",
+    "存储记录仪的原理" => "https://www.hioki.cn/html/methods/rec.html"
   },
   "23"=> {
     "LCR/阻抗分析仪的原理、使用方法等"=> "https://www.hioki.cn/html/methods/power.html",
@@ -12,6 +12,9 @@ IntroLinks = {
   },
   "28"=> {
     "使用方法"=> "https://www.hioki.cn/html/methods/current.html"
+  },
+  "53"=> {
+    "维护指南"=> "https://www.hioki.cn/html/methods/gf_lighting.html"
   },
   "10"=> {
      "温度、噪音、照度计等的使用方法"=> "https://www.hioki.cn/html/methods/enviro.html",
